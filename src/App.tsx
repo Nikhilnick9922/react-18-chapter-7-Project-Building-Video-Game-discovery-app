d11d1d7979a46205cc76ab89e765f5d458e3cb70
Nicks <nikhilpatil300@gmail.com>
2024-03-05 11:46:50 +0530

    added chakra ui - removed css code from index.css and added button for tesing chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <Button colorScheme="blue">Button</Button>; // we see ugly outline , this is not comming from chakra
+  // this is default coming from `vit project` , delete styles from index.css
 }
 
-export default App
+export default App;
+
+//  got to chakra and get started and vit => copy and install dependencies
+
+// next is proivder setup -> in main.tsx and import and wrap app
+
+// in upcoming we gonna learn about `providers` when we learn about advanced topics
+
+// we gonna add button for testing of chakra ui
